61c0d97a800e76512863a28f30bb8528bcee89b5
kalebe <kalebe.klc>
2022-12-08 20:21:09 -0300

    (#53) criacao das main da api - https://github.com/fga-eps-mds/2022.2-Amis-Doc/issues/53
    Co-authored-by: fxred <fxr.ed03@gmail.com>
    Co-authored-by: Thiago Luiz <thiago.luiz20000@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI, status
 app = FastAPI()
 
-#app.include_router(aluna_router)
+# Routers
+from alunas.router import router as aluna_router
 
-@app.get("/api/hello_world",status_code=status.HTTP_201_CREATED)
+from config import settings
+
+app = FastAPI()
+
+app.include_router(aluna_router)
+
+@app.get('/')
 async def hello_world():
-    return {"message": "Hello World"}
+    return {
+        "db_type": settings.db_type,
+        "db_url": settings.db_url,
+    }
