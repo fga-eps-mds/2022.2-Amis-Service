b78850acb136e911894e3e9be617efea16fa80ac
thiagomesUNB <thiago.luiz20000@gmail.com>
2022-12-07 21:30:11 -0300

    (#53): Start dos arquivos https://github.com/fga-eps-mds/2022.2-Amis-Doc/issues/53

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, status
 app = FastAPI()
 
+#app.include_router(aluna_router)
+
 @app.get("/api/hello_world",status_code=status.HTTP_201_CREATED)
 async def hello_world():
     return {"message": "Hello World"}
