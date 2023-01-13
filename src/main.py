a1e0126da60683433188128f383e4f0801c04262
Thiago <thiago.luiz20000@gmail.com>
2023-01-13 14:15:38 -0300

    (#102): Corrigindo bugs no docker https://github.com/fga-eps-mds/2022.2-Amis-Doc/issues/102
    Co-authored-by: André Lucas <andre.lucas.lemos@hotmail.com>

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ app = FastAPI()
 # Routers
 from .assistentes.router import router as assistentes_router
 from .alunas.router import router as aluna_router
+from .turmas.router import router as turma_router
 
 from .config import settings
 
@@ -11,6 +12,7 @@ app = FastAPI()
 
 app.include_router(aluna_router)
 app.include_router(assistentes_router)
+app.include_router(turma_router)
 
 @app.get('/')
 async def hello_world():
