acea9b8aaedbf07aa56a103c8720133a9081e17c
KalebeLopes <calbkalebe@gmail.com>
2023-01-17 13:27:52 -0300

    (#106) Criando a tablema Matricula e unificando as models - https://github.com/fga-eps-mds/2022.2-Amis-Doc/issues/106
    Co-authored-by: Fabricio Queiroz <fabriciodequeiroz2016@gmail.com>
    Co-authored-by: Paulo Borba <paulohbr18@gmail.com>

--- src/turmas/model.py
+++ src/turmas/model.py
@@ -1,16 +1,30 @@
-'''Importando parâmetros da orm'''
-from sqlalchemy import Column, Integer, String, Enum
-from ..database import Base
+# '''Importando parâmetros da orm'''
+# from sqlalchemy import Column, Integer, String, Enum
+# from sqlalchemy.orm import relationship
+# from ..matricula.model import matricula
 
-class Turmas(Base):
-    '''Classe para estabelecer o modelo da tabela na DB'''
-    __tablename__ = "turmas"
+# # from src.alunas.model import Alunas
+# from ..database import Base
+# # from ..database import engine
+# # from ..matricula.model import matricula
 
-    id: int = Column(Integer, primary_key = True, index = True)
-    descricao: str = Column(String(100), nullable = True)
-    turno: str = Column(Enum('1','2','3'), nullable = False) #Verificar se troca para matutino,vespertino e noturno
-    capacidade: int = Column(Integer, nullable = False)
-    horarioInicio: str = Column(String(100), nullable = True) # alterar para time
-    horarioFim: str = Column(String(100), nullable = True) # alterar para time
-    dataInicio: str = Column(String(100), nullable = True) # alterar para time
-    dataFim: str = Column(String(100), nullable = True) # alterar para time
+# class Turmas(Base):
+#     '''Classe para estabelecer o modelo da tabela na DB'''
+#     __tablename__ = "turmas"
+#     # __table_args__ = {"schema": "public"}
+
+#     idTurmas: int = Column(Integer, primary_key = True, index = True)
+#     descricao: str = Column(String(100), nullable = True)
+#     # turno: str = Column(Enum('1','2','3'), nullable = False) #Verificar se troca para matutino,vespertino e noturno
+#     # capacidade: int = Column(Integer, nullable = False)
+#     # horarioInicio: str = Column(String(100), nullable = True) # alterar para time
+#     # horarioFim: str = Column(String(100), nullable = True) # alterar para time
+#     # dataInicio: str = Column(String(100), nullable = True) # alterar para time
+#     # dataFim: str = Column(String(100), nullable = True) # alterar para time
+
+#     aluna = relationship("Alunas", secondary=matricula, back_populates="turma")
+
+#     # def __repr__(self):
+#     #     return f"<Turmas {self.name}>"
+
+# # Base.metadata.create_all(engine)
