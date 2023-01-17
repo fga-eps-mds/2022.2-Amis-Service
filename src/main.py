857842285dc708242b495551db4b8d9ab27fb123
KalebeLopes <calbkalebe@gmail.com>
2023-01-17 13:39:19 -0300

    (#106) Ajustando importações das rotas - https://github.com/fga-eps-mds/2022.2-Amis-Doc/issues/106
    Co-authored-by: Fabricio Queiroz <fabriciodequeiroz2016@gmail.com>
    Co-authored-by: Paulo Borba <paulohbr18@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,17 +1,30 @@
 from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+# UTILIZAR PARA RODAR LOCALMENTE
+# origins = ["*"]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 # Routers
 from .assistentes.router import router as assistentes_router
 from .alunas.router import router as aluna_router
 from .turmas.router import router as turma_router
+from .matricula.router import router as matricula_router
 
 from .config import settings
 
-app = FastAPI()
-
 app.include_router(aluna_router)
+app.include_router(turma_router)
 app.include_router(assistentes_router)
+app.include_router(matricula_router)
 
 @app.get('/')
 async def hello_world():
