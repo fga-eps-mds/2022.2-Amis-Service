1bf2df4c96d9a5293efe9c479ebc8c7b524a7d04
KalebeLopes <calbkalebe@gmail.com>
2023-01-17 13:34:12 -0300

    (#106) Criando schema da Matricula - https://github.com/fga-eps-mds/2022.2-Amis-Doc/issues/106
    Co-authored-by: Fabricio Queiroz <fabriciodequeiroz2016@gmail.com>
    Co-authored-by: Paulo Borba <paulohbr18@gmail.com>

--- src/turmas/schema.py
+++ src/turmas/schema.py
@@ -3,20 +3,23 @@ from pydantic import BaseModel
 
 class TurmasBase(BaseModel):
     '''Classe para definir os modelos recebidos na API'''
+    # UTILIZAR PARA TESTES LOCAIS
     descricao: str 
-    turno: str 
-    capacidade: int 
-    horarioInicio: str  
-    horarioFim: str  
-    dataInicio: str  
-    dataFim: str
+
+    # UTILIZAR PARA SUBIR PARA PRODUÇÃO
+    # descricao: str 
+    # turno: str 
+    # capacidade: int 
+    # horarioInicio: str  
+    # horarioFim: str  
+    # dataInicio: str  
+    # dataFim: str
     
 class TurmasRequest(TurmasBase):
     '''...'''
 
 class TurmasResponse(TurmasBase):
     '''...'''
-    turno: str
     id: int
     class Config:
         orm_mode = True
