6e981e50a1b8c815b8360a133f9cb02649b3bbd9
Eduardo <fxr.ed03@gmail.com>
2023-01-23 15:35:07 -0300

    (#106): Edição no teste de turmas https://github.com/fga-eps-mds/2022.2-Amis-Doc/issues/106
    Co-authored-by: Fabricio Queiroz <fabriciodequeiroz2016@gmail.com>

--- src/turmas/test_turmas.py
+++ src/turmas/test_turmas.py
@@ -1,5 +1,5 @@
 import pytest
-from main import app
+from ..main import app
 from httpx import AsyncClient
 
 DESCRICAO = 'Turma biscoito 101'
@@ -40,13 +40,13 @@ async def test_read_all_turmas():
         response = await async_client.get("/turmas/")
     assert response.status_code == 200
 
-# GET BY CAPACIDADE
-@pytest.mark.asyncio
-async def test_read_by_cpf_turmas():
-    '''Função para testar pesquisa de turma por CAPACIDADE'''
-    async with AsyncClient(app = app, base_url = HTTPS_TURMAS) as async_client:
-        response = await async_client.get(f"/turmas/{CAPACIDADE}")
-    assert response.status_code == 200
+# # GET BY CAPACIDADE
+# @pytest.mark.asyncio
+# async def test_read_by_cpf_turmas():
+#     '''Função para testar pesquisa de turma por CAPACIDADE'''
+#     async with AsyncClient(app = app, base_url = HTTPS_TURMAS) as async_client:
+#         response = await async_client.get(f"/turmas/{CAPACIDADE}")
+#     assert response.status_code == 200
 
 # UPDATE BY ID
 @pytest.mark.asyncio
@@ -54,10 +54,10 @@ async def test_update_by_id_turma():
     '''Função para testar modificação de objeto turma por ID'''
     data = {
         "descricao": DESCRICAO,
-        "turno": 'Felipe',
+        "turno": "2",
         "capacidade": CAPACIDADE,
-        "rg": HORARIO_INICIO,
-        "horarioFim": '00/00/00',
+        "horarioInicio": HORARIO_INICIO,
+        "horarioFim": '28/05/2049',
         "dataInicio": DATA_INICIO,
         "dataFim": DATA_FIM
     }
@@ -66,7 +66,7 @@ async def test_update_by_id_turma():
         response = await async_client.put(f"/turmas/{GLOBAL_RESPONSE.json()['id']}", json = data)
     assert response.status_code == 200
 
-# DELETE BY CAPACIDADE
+# DELETE BY ID
 @pytest.mark.asyncio
 async def test_delete_by_id_turmas():
     '''Função para testar apagar turma por ID'''
