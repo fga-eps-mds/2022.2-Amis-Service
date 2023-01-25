1cf73d0e23ccb55c631bcd4a506c5bd7b0ee1fac
KalebeLopes <calbkalebe@gmail.com>
2023-01-25 00:22:08 -0300

    Adicionando teste turmas
    Co-authored-by: Thiago Luiz <thiago.luiz20000@gmail.com>
    Co-authored-by: Caio Sulz <caiosulz14@gmail.com>

--- src/turmas/test_turmas.py
+++ src/turmas/test_turmas.py
@@ -40,23 +40,15 @@ async def test_read_all_turmas():
         response = await async_client.get("/turmas/")
     assert response.status_code == 200
 
-# GET BY CAPACIDADE
-@pytest.mark.asyncio
-async def test_read_by_cpf_turmas():
-    '''Função para testar pesquisa de turma por CAPACIDADE'''
-    async with AsyncClient(app = app, base_url = HTTPS_TURMAS) as async_client:
-        response = await async_client.get(f"/turmas/{CAPACIDADE}")
-    assert response.status_code == 200
-
 # UPDATE BY ID
 @pytest.mark.asyncio
 async def test_update_by_id_turma():
     '''Função para testar modificação de objeto turma por ID'''
     data = {
         "descricao": DESCRICAO,
-        "turno": 'Felipe',
+        "turno": '1',
         "capacidade": CAPACIDADE,
-        "rg": HORARIO_INICIO,
+        "horarioInicio": HORARIO_INICIO,
         "horarioFim": '00/00/00',
         "dataInicio": DATA_INICIO,
         "dataFim": DATA_FIM
