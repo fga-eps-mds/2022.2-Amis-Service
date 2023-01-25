f25c46d930f7971a03c0e3e9cefd6822778c0601
João Pedro <jps12carvalho@gmail.com>
2023-01-25 12:07:55 -0300

    Aumentando o tamanho do pool do banco de dados

--- src/database.py
+++ src/database.py
@@ -6,7 +6,9 @@ from .config import settings
 SQLALCHEMY_DATABASE_URL = settings.db_connect_url
 print(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL,
+    pool_size=20,
+    max_overflow=0
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
