c1fa8e9036e52929e1688dfd9b177fd608f07cca
jvsoutomaior <soutomaior950@gmail.com>
2023-01-30 23:52:34 -0300

    (#115) testes de receitas
    Co-authored-by: paulohborba <paulohbr18@gmail.com>
    
    https://github.com/fga-eps-mds/2022.2-Amis-Doc/issues/115

--- src/receita/test_receita.py
+++ src/receita/test_receita.py
@@ -1,11 +1,6 @@
 import pytest 
 from ..main import app
 from httpx import AsyncClient
-from .repository import ReceitasRepository
-from ..database import engine, Base, get_db
-from sqlalchemy.orm import Session
-
-Base.metadata.create_all(bind=engine)
 
 NOME_RECEITA = 'Bolo de cenoura'
 INGREDIENTES = 'Farinha'
@@ -18,21 +13,51 @@ HTTPS_RECEITA = "https://receita"
 @pytest.mark.asyncio
 async def test_create_receita():
     '''Função para testar a criação de uma receita'''
-data = {
-    "nome": NOME_RECEITA,
-    "ingredientes": INGREDIENTES,
-    "modo_preparo": MODO_PREPARO
-}
-async with AsyncClient( app = app, base_url = HTTPS_RECEITA) as async_client:
-    response = await async_client.post("/receita/", json=data)
-    global GLOBAL_RESPONSE
-    GLOBAL_RESPONSE= response
-assert response.status_code == 201
+    data = {
+        "nome": NOME_RECEITA,
+        "ingredientes": INGREDIENTES,
+        "modo_preparo": MODO_PREPARO
+    }
+    async with AsyncClient( app = app, base_url = HTTPS_RECEITA) as async_client:
+        response = await async_client.post("/receita/", json=data)
+        global GLOBAL_RESPONSE
+        GLOBAL_RESPONSE = response
+    assert response.status_code == 201
 
 # GET ALL
 @pytest.mark.asyncio
-async def test_read_all_alunas():
+async def test_read_all_recitas():
     '''Função para testar exibição de todas receitas'''
     async with AsyncClient(app = app, base_url = HTTPS_RECEITA) as async_client:
-        response = await async_client.get("/alunas/")
+        response = await async_client.get("/receita/")
     assert response.status_code == 200
+
+# GET BY ID
+@pytest.mark.asyncio
+async def test_read_by_id_receitas():
+    '''Função para testar pesquisa de receitas por id'''
+    async with AsyncClient(app = app,base_url = HTTPS_RECEITA) as async_client:
+        response = await async_client.get(f"/receita/{id}")
+    assert response.status_code == 200
+
+# UPDATE BY ID
+@pytest.mark.asyncio
+async def test_update_by_id_receita():
+    '''Função para testar modificação de objeto receita por ID'''
+    data = {
+        "nome": NOME_RECEITA,
+        "ingredientes": INGREDIENTES,
+        "modo_preparo": MODO_PREPARO
+    }
+
+    async with AsyncClient(app = app, base_url = HTTPS_RECEITA) as async_client:
+        response = await async_client.put(f"/receita/{GLOBAL_RESPONSE.json()['id']}", json = data)
+    assert response.status_code == 200
+
+# DELETE POR ID
+@pytest.mark.asyncio
+async def test_delete_by_id_receitas():
+    '''Função para testar apagar receita por ID'''
+    async with AsyncClient(app = app, base_url = HTTPS_RECEITA) as async_client:
+        response = await async_client.delete(f"/receita/{GLOBAL_RESPONSE.json()['id']}")
+    assert response.status_code == 204
